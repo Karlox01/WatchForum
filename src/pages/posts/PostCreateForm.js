05bda44190e934fa2c109d4530c79a48b5d6b906
Karlox01 <orlandinikarlo@gmail.com>
2024-01-15 19:29:01 +0000

    add react quill to the create post option to align better with the forum vision when creating editing posts

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
-
+import React, { useRef, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 import Upload from "../../assets/upload.png";
 
@@ -13,85 +16,153 @@ import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
 
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+
 function PostCreateForm() {
-
     const [errors, setErrors] = useState({});
-
     const [postData, setPostData] = useState({
         title: "",
         content: "",
         image: "",
     });
     const { title, content, image } = postData;
+    const imageInput = useRef(null);
+    const history = useHistory();
 
-    const handleChange = (event) => {
+    const handleChange = (value) => {
         setPostData({
             ...postData,
-            [event.target.name]: event.target.value,
+            content: value,
         });
     };
 
+    const handleChangeImage = (event) => {
+        if (event.target.files.length) {
+            URL.revokeObjectURL(image);
+            setPostData({
+                ...postData,
+                image: URL.createObjectURL(event.target.files[0]),
+            });
+        }
+    };
 
-    const textFields = (
-        <div className="text-center">
-            <Form.Group>
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                    type="text"
-                    name="title"
-                    value={title}
-                    onChange={handleChange}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Content</Form.Label>
-                <Form.Control
-                    as="textarea"
-                    rows={6}
-                    name="content"
-                    value={content}
-                    onChange={handleChange}
-                />
-            </Form.Group>
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
 
-            <Button
-                className={`${btnStyles.Button} ${btnStyles.Blue}`}
-                onClick={() => { }}
-            >
-                cancel
-            </Button>
-            <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-                create
-            </Button>
-        </div>
-    );
+        formData.append("title", title);
+        formData.append("content", content);
+        formData.append("image", imageInput.current.files[0]);
+
+        try {
+            const { data } = await axiosReq.post("/posts/", formData);
+            history.push(`/posts/${data.id}`);
+        } catch (err) {
+            console.log(err);
+            if (err.response?.status !== 401) {
+                setErrors(err.response?.data);
+            }
+        }
+    };
+
+    const openFileInput = () => {
+        imageInput.current.click();
+    };
 
     return (
-        <Form>
-            <Row>
-                <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
-                    <Container
-                        className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
-                    >
-                        <Form.Group className="text-center">
-
-                            <Form.Label
-                                className="d-flex justify-content-center"
-                                htmlFor="image-upload"
-                            >
-                                <Asset src={Upload} message="Click to upload an image" />
-                            </Form.Label>
-
+        <Form onSubmit={handleSubmit}>
+            <Container className={`${appStyles.Content} ${styles.Container}`}>
+                <Row className="justify-content-center mt-4">
+                    <Col xs={12} md={8}>
+                        <Form.Group>
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="title"
+                                value={title}
+                                onChange={handleChange}
+                            />
                         </Form.Group>
-                        <div className="d-md-none">{textFields}</div>
-                    </Container>
-                </Col>
-                <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
-                    <Container className={appStyles.Content}>{textFields}</Container>
-                </Col>
-            </Row>
+                        {errors?.title?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+                        <Form.Group>
+                            <Form.Label>Content</Form.Label>
+                            <ReactQuill
+                                className={appStyles.quill}
+                                value={content}
+                                onChange={handleChange}
+                                modules={PostCreateForm.modules}
+                            />
+                        </Form.Group>
+                        {errors?.content?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+                    </Col>
+                </Row>
+                <Row className="justify-content-center mt-4">
+                    <Col xs={12} md={8} className="text-center">
+                        {image ? (
+                            <figure>
+                                <Image className={appStyles.Image} src={image} rounded />
+                            </figure>
+                        ) : (
+                            <Button
+                                className={`${btnStyles.Button} ${btnStyles.Bright}`}
+                                onClick={openFileInput}
+                            >
+                                Upload Image
+                            </Button>
+                        )}
+                        <Form.File
+                            id="image-upload"
+                            accept="image/*"
+                            onChange={handleChangeImage}
+                            ref={imageInput}
+                            className="d-none"
+                        />
+                        {errors?.image?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+                    </Col>
+                </Row>
+                <Row className="justify-content-center mt-4">
+                    <Col xs={12} md={8} className="text-center">
+                        <Button
+                            className={`${btnStyles.Button} ${btnStyles.Bright}`}
+                            onClick={() => history.goBack()}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            className={`${btnStyles.Button} ${btnStyles.Bright}`}
+                            type="submit"
+                        >
+                            Create
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
         </Form>
     );
 }
+
+PostCreateForm.modules = {
+    toolbar: [
+        [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+        [{ size: [] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'color': [] }, { 'background': [] }],
+        ['link', 'image', 'video'],
+        ['clean'],
+    ],
+};
 
 export default PostCreateForm;
